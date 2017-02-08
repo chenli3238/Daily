da2664b4d1d6517664429a3f6255f11e183c1a92
WqyJh <781345688@qq.com>
2017-02-09 00:33:18 +0800

    Failed to add an dialog with Day picker and time picker

---
+++ app/src/main/java/com/wqy/daily/widget/NoticeDialogFragment.java
@@ -0,0 +1,10 @@
+package com.wqy.daily.widget;
+
+import android.support.v4.app.DialogFragment;
+
+/**
+ * Created by wqy on 17-2-8.
+ */
+
+public class NoticeDialogFragment extends DialogFragment {
+}
